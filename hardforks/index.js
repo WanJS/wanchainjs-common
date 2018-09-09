b72dc20c851fe1c88229ea691ddd6cde94197455
Peter van Mourik <tyrion70@users.noreply.github.com>
2018-09-09 17:28:17 +0200

    0.5.1 Wanchain changes (#1)
    * 0.5.1 Wanchain changes

--- hardforks/index.js
+++ hardforks/index.js
@@ -1,12 +1,5 @@
 const hardforkChanges = [
-  [ 'chainstart', require('./chainstart.json') ],
-  [ 'homestead', require('./homestead.json') ],
-  [ 'dao', require('./dao.json') ],
-  [ 'tangerineWhistle', require('./tangerineWhistle.json') ],
-  [ 'spuriousDragon', require('./spuriousDragon.json') ],
-  [ 'byzantium', require('./byzantium.json') ],
-  [ 'constantinople', require('./constantinople.json') ],
-  [ 'hybridCasper', require('./hybridCasper.json') ]
+  [ 'byzantium', require('./byzantium.json') ]
 ]
 
 module.exports = hardforkChanges
